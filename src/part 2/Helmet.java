9a58ff07982315ded946e5d7f90d0606b65362c0
Fabricio Almeida <169138021+Fabs1999@users.noreply.github.com>
2024-11-18 18:43:24 +0000

    Update Helmet.java
    I'm trying to add JavaDoc on the classes and objects that i created, ensuring that is all indented

--- src/part 2/Helmet.java
+++ src/part 2/Helmet.java
@@ -5,8 +5,19 @@ public class Helmet implements Headgear {
     private int weightInGrammes;
     private boolean safetyCertified;
 
+    /**
+     * With this class the objective was to create a Helmet Object with specific weight
+     * and safety certification.
+     *
+     * @param weightInGrammes
+     * @param safetyCertified
+     * @throws IllegalAccessException
+     */
     public Helmet(int weightInGrammes, boolean safetyCertified) throws IllegalAccessException {
-        if (weightInGrammes <= 0) throw new IllegalAccessException("Weight must be > 0");
+        // Ensuring that the weight is greater than 0
+        if (weightInGrammes <= 0) {
+            throw new IllegalAccessException("Weight must be > 0");
+        }
         this.weightInGrammes = weightInGrammes;
         this.safetyCertified = safetyCertified;
     }
@@ -27,12 +38,13 @@ public class Helmet implements Headgear {
 
         return this.safetyCertified;
     }
-
+    // Tester!!
     public class HeadgearTest {
 
         public static void main(String [] args) throws IllegalAccessException {
+            // creating a helmet object
             Helmet helmet = new Helmet(500, true);
-
+            // print out the properties
             System.out.println("Weight: " + helmet.getWeight());
             System.out.println("Type: " + helmet.getType());
             System.out.println("Safety Certified: " + helmet.isSafetyCertified());
