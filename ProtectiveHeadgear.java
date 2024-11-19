e332255591903a9f69c8077d10202c69a2dfa973
Fabricio Almeida <169138021+Fabs1999@users.noreply.github.com>
2024-11-19 20:07:53 +0000

    Update ProtectiveHeadgear.java
    Comments on this class to explain the line of thought

--- ProtectiveHeadgear.java
+++ ProtectiveHeadgear.java
@@ -3,6 +3,12 @@ import src.Headgear;
 public abstract class ProtectiveHeadgear implements Headgear {
     private double protectionFactor;
 
+    /**
+     * Constructs a protectiveHeadgear with the specific protection factor.
+     * @param protectionFactor
+     * @throws IllegalAccessException if the protection factor is negative
+     */
+
     public ProtectiveHeadgear(double protectionFactor){
         setProtectionFactor(protectionFactor);
     }
@@ -13,6 +19,7 @@ public abstract class ProtectiveHeadgear implements Headgear {
         }
         return protectionFactor;
     }
+    // gets the protection factor of the headgear
     public double getProtectionFactor(){
         return protectionFactor;
     }
