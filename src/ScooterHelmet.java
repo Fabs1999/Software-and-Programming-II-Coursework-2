189b71f33ce557581f7d8a6859a545f9b6b12b83
Fabricio Almeida <169138021+Fabs1999@users.noreply.github.com>
2024-11-20 09:03:21 +0000

    Scooter Helmet Comments

--- src/ScooterHelmet.java
+++ src/ScooterHelmet.java
@@ -4,12 +4,23 @@ public class ScooterHelmet extends ProtectiveHeadgear {
 
     private boolean hasVisor;
 
+    /**
+     * Creates a Scooter Helmet with a specific protection factor and visor open
+     *
+     * @param protectionFactor the level of protection that this helmet offers - must be positive
+     * @param hasVisor confirms if the helmet includes a visor
+     * @throws IllegalArgumentException if protection factor is invalid.
+     * @throws IllegalAccessException if protection factor is invalid.
+     */
+
     public ScooterHelmet(double protectionFactor, boolean hasVisor) throws IllegalArgumentException, IllegalAccessException {
         super(protectionFactor);
         this.hasVisor = hasVisor;
     }
     @Override
     public double computeValue(){
+        // calculates the value of this scooter helmet.
+        // the value it will depend on the protection and if it has a value.
         int c1= hasVisor ? 160 : 80;
         int c2 = 400;
         return c1 + getProtectionFactor() * c2;
@@ -21,22 +32,22 @@ public class ScooterHelmet extends ProtectiveHeadgear {
                 getProtectionFactor() + ", value: " + computeValue();
     }
 
-    @Override
+
     public int getWeight() {
         return 0;
     }
 
-    @Override
+
     public String getType() {
         return "";
     }
 
-    @Override
+
     public boolean isSafetyCertified() {
         return false;
     }
 
-    @Override
+
     public double valueComputed() {
         return 0;
     }
